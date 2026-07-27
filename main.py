f5b1f5f8b78b0d6597191a75c656c50bfe8ed8ec
dwayne-desmarais <60883098+dwayne-desmarais@users.noreply.github.com>
2026-07-27 19:12:11 -0400

    Initialize API Troubleshooting Toolkit
    Added a basic structure for an API troubleshooting toolkit.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import requests
 
+
+def main():
+    print("================================")
+    print(" API Troubleshooting Toolkit")
+    print("================================")
+
+    url = input("Enter an API URL: ")
+
+    print(f"\nTesting {url}...\n")
+
+
+if __name__ == "__main__":
+    main()
